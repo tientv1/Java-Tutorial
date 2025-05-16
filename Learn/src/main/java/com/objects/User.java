9f16961d0084f3a2ad5a4c2becec59f5c133df2c
Tien Tran Van <tientranvan19052004@gmail.com>
2025-05-16 14:10:24 +0700

    Create User.java

---
+++ Learn/src/main/java/com/objects/User.java
@@ -0,0 +1,14 @@
+package com.objects;
+
+public class User {
+    private String name;
+    public int age;
+
+    public String setName (String name) {
+        return this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
