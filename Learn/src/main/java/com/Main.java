9612e590b9f6a8099e8d5787ca33eed811995481
Tien Tran Van <tientranvan19052004@gmail.com>
2025-05-16 14:10:22 +0700

    Update Main.java

--- Learn/src/main/java/com/Main.java
+++ Learn/src/main/java/com/Main.java
@@ -1,8 +1,14 @@
 package com;
 
+import com.objects.User;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        User user = new User("Tom", 20);
+        User user1 = new User("Tom1", 20);
+        User user2 = new User("Tom2", 20);
+        User[] users = {user, user1, user2};
+        System.out.println(User.create(users, 3));
     }
 }
